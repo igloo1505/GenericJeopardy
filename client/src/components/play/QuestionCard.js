6cb0a7847fdbc8290f3567c8116b33e08eca58ec
Andrew Mueller <iglooworks1505@gmail.com>
2020-01-12 15:34:19 -0600

    added some styling to question selected card, still need to make buttons manipulate score state appropiately

--- client/src/components/play/QuestionCard.js
+++ client/src/components/play/QuestionCard.js
@@ -13,40 +13,45 @@ const QuestionCard = ({ ...props }) => {
 
   console.log(question);
 
-  // const { _id, category, points, answer } = question;
+  const showAnswer = e => {
+    console.log("Spacebar Pressed");
+  };
 
   return (
     <Fragment>
       <h1>Switched to card here</h1>
       <button onClick={() => props.grid()}>Go Back</button>
+
+      <div className="Questioncard bg-light">
+        <h3 className="text-primary text-left question">{question}</h3>
+        <h5 className="text-dark text-left answer">{answer}</h5>{" "}
+        <button
+          className="btn btn-success btn-sm TeamTwoUp"
+          onClick={showAnswer}
+        >
+          Team Two +
+        </button>
+        <button
+          className="btn btn-danger btn-sm TeamTwoDown"
+          onClick={showAnswer}
+        >
+          Team Two -
+        </button>
+        <button
+          className="btn btn-danger btn-sm TeamOneDown"
+          onClick={showAnswer}
+        >
+          Team One -
+        </button>
+        <button
+          className="btn btn-success btn-sm TeamOneUp"
+          onClick={showAnswer}
+        >
+          Team One +
+        </button>
+      </div>
     </Fragment>
-
-    // <div className="card bg-light">
-    //   <h3 className="text-primary text-left question">{question.question}</h3>
-    //   <h5 className="text-dark text-left answer">{answer}</h5>
-    //   <div style={{ float: "right" }} className="badge badge-success points">
-    //     {points}
-    //   </div>
-
-    //   <div style={{ float: "right" }} className="badge badge-light category">
-    //     {category}
-    //   </div>
-
-    //   <button
-    //     className="btn btn-dark btn-sm edit"
-    //     onClick={() => setCurrent(question)}
-    //   >
-    //     Edit
-    //   </button>
-    //   <button className="btn btn-danger btn-sm delete" onClick={onDelete}>
-    //     Delete
-    //   </button>
-    // </div>
   );
-};
-
-QuestionCard.propTypes = {
-  question: PropTypes.object.isRequired
 };
 
 export default QuestionCard;
