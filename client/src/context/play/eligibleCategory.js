43734bd67c6f0e08e187646d30582fa12afda19a
Andrew Mueller <iglooworks1505@gmail.com>
2020-01-18 16:34:02 -0600

    FINALLY got this array filter to work

--- client/src/context/play/eligibleCategory.js
+++ client/src/context/play/eligibleCategory.js
@@ -7,13 +7,19 @@ const eligibleCategory = questions => {
   var grouped = _.groupBy(questions, "category");
 
   let GroupedArray = Object.values(grouped);
-  console.log(GroupedArray);
+  // console.log(GroupedArray);
 
   for (var index = 0; index < GroupedArray.length; index++) {
     FilteredArray.push(_.uniqBy(GroupedArray[index], "points"));
   }
+  for (var x = 0; x < FilteredArray.length; x++) {
+    if (FilteredArray[x].length >= 5) {
+      CategoryArray.push(FilteredArray[x][0].category);
+      // console.log(FilteredArray[x][0].category);
+    }
+  }
 
-  return console.log(FilteredArray);
+  return console.log(CategoryArray);
 };
 
 export default eligibleCategory;
